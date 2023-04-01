2275d466936810efe0c91d9688b354a6a4e8f35f
Mickey Brown <mickisbrown10@gmail.com>
2023-04-01 14:18:49 -0600

    updating the profile page to use flask backend for updating profile instead of realtime DB

--- backend/server.py
+++ backend/server.py
@@ -1,22 +1,54 @@
 from flask import Flask, request
 import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("serviceAccountKey.json")
+app = firebase_admin.initialize_app(cred)
+firestore_client = firestore.client()
 
 app = Flask(__name__)
 
 
-@app.route('/profile')
-def hello():
+@app.route('/profile', methods=['GET', 'POST'])
+def profile():
     uid = request.args.get('uid')
-    print(uid)
-    response_body = {
-        "name": str(uid),
-        "about" :"Hello! I'm a full stack developer that loves python and javascript"
-    }
+    if request.method == 'GET':
+        doc_ref = firestore_client.collection('users').document(uid)
+        user = doc_ref.get()
+        return user.to_dict()
+    else:
+        print("test", request.args.get('platform'))
+        user = {
+            'about_me': request.args.get('bio'),
+            'age': request.args.get('age'),
+            'category': request.args.get('category'),
+            'city': request.args.get('city'),
+            'name': request.args.get('name'),
+            'platform': request.args.get('platform'),
+            'state': request.args.get('state'),
+        }
+        doc_ref = firestore_client.collection('users').document(uid)
+        doc_ref.set(user)
 
-    return response_body
+        user = doc_ref.get()
+        return user.to_dict()
 
-@app.route('/auth', methods=['GET'])
+@app.route('/auth', methods=['POST'])
 def auth():
-    email, password = request.args.get('email'), request.args.get('password')
+    email, uid, name = request.args.get('email'), request.args.get('uid'), request.args.get('name')
     #Create an a new user under the profile collection with the uid as the document name
+    user = {
+        'email': email,
+        'uid': uid,
+        'about_me': '',
+        'age': '',
+        'avi': '',
+        'category': '',
+        'city': '',
+        'name': name,
+        'platform': '',
+        'state': '',
+    }
+    doc_ref = firestore_client.collection('users').document(uid)
+    doc_ref.set(user)
     return "Sign Up"
